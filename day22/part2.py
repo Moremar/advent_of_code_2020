31e16f4219872931d4969d45d0ed30d9e8ca1da9
chevallier <chevallier.th@gmail.com>
2020-12-22 15:35:11 +0900

    day 22 variable rename

--- day22/part2.py
+++ day22/part2.py
@@ -10,7 +10,7 @@ def play_game(deck1, deck2):
     while len(deck1) and len(deck2):
         config = serialize_config(deck1, deck2)
         if config in seen_configs:
-            return (0, deck1, deck2)
+            return (1, deck1, deck2)
         seen_configs.add(config)
         card1, card2 = deck1.pop(0), deck2.pop(0)
         if len(deck1) < card1 or len(deck2) < card2:
@@ -20,18 +20,18 @@ def play_game(deck1, deck2):
             else:
                 deck2 += [card2, card1]
         else:
-            # determine winner by recursion
-            winner, _, _ = play_game(list(deck1[:card1]), list(deck2[:card2]))
-            if winner == 0:
+            # determine round winner by recursion
+            player, _, _ = play_game(list(deck1[:card1]), list(deck2[:card2]))
+            if player == 1:
                 deck1 += [card1, card2]
             else:
                 deck2 += [card2, card1]
-    return (0 if len(deck1) else 1, deck1, deck2)
+    return (1 if len(deck1) else 2, deck1, deck2)
 
 
 def solve(decks):
-    winner, deck1, deck2 = play_game(*decks)
-    winner = deck1 if winner == 0 else deck2
+    player, deck1, deck2 = play_game(*decks)
+    winner = deck1 if player == 1 else deck2
     return sum([i * winner[len(winner)-i] for i in range(1, len(winner)+1)])
 
 
